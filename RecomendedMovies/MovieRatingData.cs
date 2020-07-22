31c8cc54919b635ac8eb1ed22648bd63653913e9
unknown <michaelrafaelventura01@gmail.com>
2020-07-21 19:51:58 -0700

    tarea finalizada inteligencia artificial

---
+++ RecomendedMovies/MovieRatingData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace RecomendedMovies
+{
+    public class MovieRating
+    {
+        [LoadColumn(0)]
+        public float userId;
+
+        [LoadColumn(1)]
+        public float movieId;
+
+        [LoadColumn(2)]
+        public float Label;
+    }
+
+    public class MovieRatingPrediction
+    {
+        public float Label;
+        public float Score;
+    }
+
+}
